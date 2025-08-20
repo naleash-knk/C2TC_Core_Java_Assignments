f318dc99bfc3635f9b799d7a91ee43f233ff665d
naleash-knk <naleashwarakumaran@gmail.com>
2025-08-20 10:56:22 +0530

    assignment 3 updated

--- src/assignment3/Composition/CompositionMain.java
+++ src/assignment3/Composition/CompositionMain.java
@@ -6,10 +6,13 @@ public class CompositionMain {
 
         Item book = new Item(12457, "DSA Algorithms", "Naleash");
         Item magazine = new Item(87542, "Journey of Charles", 88);
+        Item tamizh = new Item(202206023,"Tamzhselvan","Boppins");
 
         lib.addItem(book);
         lib.addItem(magazine);
-
+        lib.addItem(tamizh);
+        
+        System.out.println("Library Contains: ");
         lib.displayAllItems();
     }
 }
